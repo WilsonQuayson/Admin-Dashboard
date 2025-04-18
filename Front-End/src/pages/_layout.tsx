d56f716bbc54b4ef6fc000836aa38989c3a4269a
Wilson Quayson <wilsonquayson1999@gmail.com>
2025-04-19 00:06:38 +0200

    (+)F06: Continue implementing collapsible navbar

--- Front-End/src/pages/_layout.tsx
+++ Front-End/src/pages/_layout.tsx
@@ -14,25 +14,28 @@ const Layout = ({ children } : {children: ReactElement}) => {
                     <p className="text-neutral-600 mt-1 font-medium whitespace-nowrap">John Doe</p>
                 </section>
             </section>
-            <section className="mt-12 pl-2 flex flex-col justify-between h-4/5">
+            <section className={`mt-12 flex flex-col justify-between h-4/5 transition-all ease-in-out duration-500 ${collapse ? "" : "pl-2"}`}>
                 <section>
                     <nav className="">
                         <p className="uppercase font-medium text-neutral-400 text-sm">main</p>
                         <ul className="flex flex-col gap-6 mt-5">
                             <li>
                                 <Link href="/" className="flex flex-row gap-2 pl-4">
-                                    <svg className="w-6 h-6 text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <svg className="w-6 h-6 absolute text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
                                     </svg>
-                                    <p className="uppercase font-medium text-neutral-500">Dashboard</p>
+                                    {
+                                        // FIX SIDEBAR SEPERATION WHEN COLLAPSED
+                                    } 
+                                    <p className={`uppercase font-medium text-neutral-500 transition-all ease-in-out duration-500 ${collapse ? "ml-16" : "ml-8"}`}>Dashboard</p>
                                 </Link>
                             </li>
                             <li>
                                 <Link href="/students" className="flex flex-row gap-2 pl-4">
-                                    <svg className="w-6 h-6 text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <svg className="w-6 h-6 absolute text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v15a1 1 0 0 0 1 1h15M8 16l2.5-5.5 3 3L17.273 7 20 9.667"/>
                                     </svg>
-                                    <p className="uppercase font-medium text-neutral-500">Students</p>
+                                    <p className={`uppercase font-medium text-neutral-500 transition-all ease-in-out duration-500 ${collapse ? "ml-16" : "ml-8"}`}>Students</p>
                                 </Link>
                             </li>
                         </ul>
@@ -42,18 +45,18 @@ const Layout = ({ children } : {children: ReactElement}) => {
                         <ul className="flex flex-col gap-6 mt-5">
                             <li>
                                 <Link href="" className="flex flex-row gap-2 pl-4">
-                                    <svg className="w-6 h-6 text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <svg className="w-6 h-6 absolute text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
                                     </svg>
-                                    <p className="uppercase font-medium text-neutral-500">Dashboard</p>
+                                    <p className={`uppercase font-medium text-neutral-500 transition-all ease-in-out duration-500 ${collapse ? "ml-16" : "ml-8"}`}>Dashboard</p>
                                 </Link>
                             </li>
                             <li>
                                 <Link href="" className="flex flex-row gap-2 pl-4">
-                                    <svg className="w-6 h-6 text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <svg className="w-6 h-6 absolute text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
                                     </svg>
-                                    <p className="uppercase font-medium text-neutral-500">Dashboard</p>
+                                    <p className={`uppercase font-medium text-neutral-500 transition-all ease-in-out duration-500 ${collapse ? "ml-16" : "ml-8"}`}>Dashboard</p>
                                 </Link>
                             </li>
                         </ul>
@@ -65,7 +68,7 @@ const Layout = ({ children } : {children: ReactElement}) => {
                         <svg className="w-6 h-6 text-neutral-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
                         </svg>
-                        <p className="uppercase font-medium text-neutral-500">Logout</p>
+                        <p className={`uppercase font-medium text-neutral-500 transition-all ease-in-out duration-500 ${collapse ? "ml-16" : "ml-8"}`}>Logout</p>
                     </button>
                 </section>
             </section>
